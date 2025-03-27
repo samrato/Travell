667ca2f419f12745afb54c39c5dd9c233f2d7e53
Willington Juma <willingtonejumamconyango@gmail.com>
2025-03-27 17:51:42 +0300

    .

--- Routes/Routes.js
+++ Routes/Routes.js
@@ -1,22 +1,23 @@
 const { Router } = require("express");
 const { RegisterUser, LoginUser } = require("../controllers/User.controllers");
-const{GetStory,AddStory,DeleteStory,UpdateStory,GetUserBook}=require("../controllers/Books.controllers")
+const {
+  GetStory,
+  AddStory,
+  DeleteStory,
+  UpdateStory,
+  GetUserBook,
+} = require("../controllers/Books.controllers");
 
-const routes=Router()
+const routes = Router();
 // the login and registrations routes
 
+routes.post("/register", RegisterUser);
+routes.post("/login", LoginUser);
+// the routes are updated
+routes.get("/story", GetStory);
+routes.post("/story", AddStory);
+routes.delete("/story/:id", DeleteStory);
+routes.get("/story/:id", GetUserBook);
+routes.patch("/story/:id", UpdateStory);
 
-routes.post("/register",RegisterUser)
-routes.post("/login",LoginUser)
-
-
-routes.get("/story",GetStory)
-routes.post("/story",AddStory)
-routes.delete("/story/:id",DeleteStory)
-routes.get("/story",GetUserBook)
-routes.patch("/story/:id",UpdateStory)
-
-
-
-
-module.exports=routes
+module.exports = routes;
